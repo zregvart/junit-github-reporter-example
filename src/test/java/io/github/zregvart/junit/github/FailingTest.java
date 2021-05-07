6466ca5a425c2b3224c4878303c933b7ac2ca0fc
Zoran Regvart <zregvart@apache.org>
2021-05-07 22:02:00 +0200

    chore: initial commit

---
+++ src/test/java/io/github/zregvart/junit/github/FailingTest.java
@@ -0,0 +1,13 @@
+package io.github.zregvart.junit.github;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class FailingTest {
+
+    @Test
+    void willFail() {
+        assertFalse(true);
+    }
+}
